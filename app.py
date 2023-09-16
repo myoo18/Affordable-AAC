5c3c2d3298bdbcfbfac9498880320f7890f3e619
Michael Yoo <111733324+myoo18@users.noreply.github.com>
2023-09-16 19:00:30 -0400

    sign in works

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 from model import generate_text
 from input import rearrange_sentence
+from pymongo import MongoClient
+import certifi
 
 app = Flask(__name__)
-app.secret_key = 'your_super_secret_key'  
+app.secret_key = 'your_super_secret_key'
+
+# MongoDB setup
+connection = 'mongodb+srv://jyee25:jyee@vthacks.lza3x1j.mongodb.net/'
+client = MongoClient(connection, tlsCAFile=certifi.where())
+#connection to users mongodb
+dbusers = client['users']
+collection = dbusers['login']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    username = session.get('username')
+    return render_template('index.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        uName = request.form["field_user"]
+        uPass = request.form["field_pass"]
+        if(collection != None):
+            print("yes")
+
+        user = collection.find_one({"user": uName})
+        if user and user['pass'] == uPass:
+            print("User authenticated!")
+            session['username'] = uName
+            session['logged in'] = True
+            return redirect(url_for('index'))
+        #user not found
+        return render_template('login.html', error=True)
+
+    return render_template('login.html')
+
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template("login.html")
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
