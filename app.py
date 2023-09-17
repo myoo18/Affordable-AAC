183cf26c26dd8279865006b8ba5f63ee134a14a9
Michael Yoo <111733324+myoo18@users.noreply.github.com>
2023-09-16 20:15:48 -0400

    merge with ivanka JS branch + login function + prediction query works

--- app.py
+++ app.py
@@ -2,36 +2,42 @@ from flask import Flask, render_template, request, url_for, redirect, flash, ses
 from model import generate_text
 from input import rearrange_sentence
 from flask_pymongo import PyMongo
-import en_core_web_sm
 from pymongo import MongoClient
 import certifi
 
+app = Flask(__name__)
+app.secret_key = 'your_super_secret_key'
 
+# MongoDB setup
 connection = 'mongodb+srv://jyee25:jyee@vthacks.lza3x1j.mongodb.net/'
 client = MongoClient(connection, tlsCAFile=certifi.where())
-# Select the correct database and collection
+#connection to mongodb
+dbusers = client['users']
+collectLogin = dbusers['login']
+dbWord = client['gridwords']
 
-db = client['gridwords']  # replace 'your_database_name' with the name of your database
-collection = db['noun']
-
-app = Flask(__name__)
-app.secret_key = 'your_super_secret_key'  
 
 @app.route('/')
 def index():
-    
+        # Create a dictionary where each key is a collection name and its value is a list of words from that collection
+    data = {}
+    for collection_name in dbWord.list_collection_names():
+        collection = dbWord[collection_name]
+        data[collection_name] = [doc.get('word', doc.get(collection_name)) for doc in collection.find({}, {"word": 1, collection_name: 1, "_id": 0}) if 'word' in doc or collection_name in doc]
     username = session.get('username')
-    return render_template('index.html', username=username)
+    return render_template('index.html', data=data, username = username)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         uName = request.form["field_user"]
         uPass = request.form["field_pass"]
-        if(collection != None):
+        if(collectLogin != None):
             print("yes")
 
-        user = collection.find_one({"user": uName})
+        user = collectLogin.find_one({"user": uName})
+        
         if user and user['pass'] == uPass:
             print("User authenticated!")
             session['username'] = uName
